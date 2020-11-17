61dbe511574498dff2a9a2b753fadd1aae81d533
981767326@qq.com <981767326@qq.com>
2020-11-17 18:32:39 +0800

    我的课程及我的收藏

--- lib/apis/mine.dart
+++ lib/apis/mine.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:molan_edu/models/CourseCollectionModel.dart';
+import 'package:molan_edu/models/MineCourseModel.dart';
 import 'package:molan_edu/models/teacherCollectionModel.dart';
 import 'package:molan_edu/utils/net/http.dart';
 import 'result.dart';
@@ -29,6 +30,21 @@ class MineApi {
       var params = {'page': page, 'list_row': listRow};
       Response res = await http.post('/teachercollectionlist', queryParameters: params);
       var result = TeacherCollectionModel.fromJson(res.data ?? {});
+      return DataResult(result, true);
+    } catch (e) {
+      return DataResult(e, false);
+    }
+  }
+
+  //我的课程
+  static Future<DataResult> mineCourseList({
+    int page,
+    int listRow,
+  }) async {
+    try {
+      var params = {'page': page, 'list_row': listRow};
+      Response res = await http.post('/mycourselist', queryParameters: params);
+      var result = MineCourseModel.fromJson(res.data ?? {});
       return DataResult(result, true);
     } catch (e) {
       return DataResult(e, false);
